9092dd79fbd19147380c6a43b74a93fbe902804e
gc-techies <geecomptech@gmail.com>
2026-04-11 14:27:55 +0100

    overriding methods, polymorphism, composition

--- OOP/prototypes/composition.js
+++ OOP/prototypes/composition.js
@@ -1,3 +1,17 @@
+const canScout = {
+    scout: function () {
+        console.log('scouting');
+    }
+}
+
+const canBet = {
+    bet: function () {
+        console.log('betting'); 
+    }
+}
+
+const target = Object.assign({}, canScout, canBet);
+
 function resetProto(Parent, Child) {
     Child.prototype = Object.create(Parent.prototype);
     Child.prototype.constructor = Child;
@@ -12,9 +26,6 @@ Beings.prototype.run = function () {
     console.log(`${this.name} running as Being`);
 };
 
-function Test(name) {
-}
-
 function Referee(name) {
     // call super constructor to initialize instance properties/members of Beings
     Beings.call(this, name);
@@ -23,12 +34,11 @@ function Referee(name) {
 // set prototype before creting instances of Referee
 resetProto(Beings, Referee);
 Referee.prototype.run = function () {
-    // use this approach if super implementation doesn't use 'this' keyword
-    Beings.prototype.run();
     // use this approach if super implementation uses 'this' keyword and you want to use the return value of super implementation
     // Beings.prototype.run.call(this);
     console.log('referee running');
 };
+// Object.assign(Referee.prototype, canBet);
 
 function Player(name) {
     // call super constructor to initialize instance properties/members of Beings
@@ -49,25 +59,12 @@ function Manager(name) {
 
 // set prototype before creting instances of Manager
 resetProto(Beings, Manager);
+Object.assign(Manager.prototype, canScout);
 
 let referee = new Referee('referee1');
 // console.log(referee.constructor);
 let ref = new Referee.prototype.constructor('referee2');
-// console.log(ref.constructor);
-// console.log(referee.constructor === ref.constructor);
-/*
-    this implies 
-    new Test.prototype.constructor('name') === new Test('name') 
-    because both are same
-*/
 
 let player = new Player('player1');
-// console.log(player.constructor);
 
 let manager = new Manager('manager1');
-
-// POLYMORPHISM
-const arr = [referee, player, manager];
-arr.forEach((being) => {
-    being.run();
-});
